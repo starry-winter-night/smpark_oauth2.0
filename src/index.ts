44e776f814e91886984d8809aacf38e705071831
smpark <smpark7723@gmail.com>
2024-07-10 19:40:17 +0900

    ADD : main index 파일 작성

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import 'reflect-metadata';
+import express from 'express';
+import MongoStore from 'connect-mongo';
+
+import env from '@configs/env';
+import configureExpress from '@configs/express';
+import { container, registerAllDependencies } from '@configs/inversify';
+import MongoDB from '@database/MongoDB';
+
+const mongoDBName = env.mongoDBName;
+
+registerAllDependencies(env.mongoDBUri, mongoDBName);
+
+const database = container.get<MongoDB>(MongoDB);
+await database.connect();
+const sessionStore = new MongoStore({
+  client: database.getClient(),
+  dbName: mongoDBName,
+  collectionName: env.mongoDBSessionCollection,
+});
+
+const app = express();
+
+await configureExpress(app, sessionStore, env);
+
+const port = env.port || 4001;
+app.listen(port, async () => {
+  console.log(`Server Connected, ${port} port!`);
+});

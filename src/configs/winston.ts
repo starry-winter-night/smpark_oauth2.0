da04636700aaf751cf21e5d761f699e3f7b9756c
smpark <smpark7723@gmail.com>
2024-07-12 22:46:35 +0900

    MODIFY : winston type

--- src/configs/winston.ts
+++ src/configs/winston.ts
@@ -19,7 +19,10 @@ if (!fs.existsSync(logDir)) {
 // 로그 포맷 정의
 const logFormat = combine(
   timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-  printf((info) => `${info.timestamp} ${info.level}: ${info.message}`),
+  printf(
+    (info: winston.Logform.TransformableInfo) =>
+      `${info.timestamp} ${info.level}: ${info.message}`,
+  ),
 );
 
 // info 레벨 파일 트랜스포트
